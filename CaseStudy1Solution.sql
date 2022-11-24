eaae8b1b9f559901f82bca6f5c2a137de1cfd74f
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-11-24 22:29:49 +0530

    Update CaseStudy1Solution.sql

--- CaseStudy1Solution.sql
+++ CaseStudy1Solution.sql
@@ -1,5 +1,11 @@
---What is the total amount each customer spent at the restaurant?
+--Q1.What is the total amount each customer spent at the restaurant?
 SELECT s.customer_id,SUM(price) FROM sales s INNER JOIN  menu m
 on s.product_id=m.product_id
+GROUP BY 1
+ORDER BY 1
+
+--Q2.How many days has each customer visited the restaurant?
+SELECT customer_id,COUNT(DISTINCT(order_date)) as Num_Days
+FROM sales
 GROUP BY 1
 ORDER BY 1
