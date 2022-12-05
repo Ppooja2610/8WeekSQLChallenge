04bfd8cc00549633b35e21dce0c5ca427f2424d3
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-12-05 14:42:10 +0530

    Update CaseStudy2-PizzaRunner.sql

--- CaseStudy2-PizzaRunner.sql
+++ CaseStudy2-PizzaRunner.sql
@@ -3,3 +3,10 @@ SELECT COUNT(pizza_id) AS Total_Pizzas FROM customer_orders
 
 --Q2.How many unique customer orders were made?
 SELECT COUNT(DISTINCT(order_id)) FROM customer_orders
+
+--Q3.How many successful orders were delivered by each runner?
+SELECT runner_id,COUNT(order_id) AS Successful_Orders
+FROM runner_orders
+WHERE cancellation IS NULL
+GROUP BY runner_id
+ORDER BY runner_id
