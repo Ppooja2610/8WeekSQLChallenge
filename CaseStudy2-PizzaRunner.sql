e1ad6c423da85c44a84f8d69b79b791180e3be84
Ppooja2610 <117577824+Ppooja2610@users.noreply.github.com>
2022-12-05 14:32:47 +0530

    Update CaseStudy2-PizzaRunner.sql

--- CaseStudy2-PizzaRunner.sql
+++ CaseStudy2-PizzaRunner.sql
@@ -1,2 +1,5 @@
 --Q1.How many pizzas were ordered?
 SELECT COUNT(pizza_id) AS Total_Pizzas FROM customer_orders
+
+--Q2.How many unique customer orders were made?
+SELECT COUNT(DISTINCT(order_id)) FROM customer_orders
